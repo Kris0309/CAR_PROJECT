29bed3d624b5c59cd0a48c180025b1df1eb2e812
Kris0309 <cx90309@gmail.com>
2025-04-08 08:49:34 +0800

    style : 排版美化

--- station_position.php
+++ station_position.php
@@ -4,7 +4,6 @@ const DB_USERNAME = "stone_test";
 const DB_PASSWORD = "Pks2pZD9LQCYLpCI75k5fw";
 const DB_NAME     = "stone_test";
 
-//建立連線
 function create_connection()
 {
     $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
@@ -15,25 +14,17 @@ function create_connection()
     return $conn;
 }
 
-//取得JSON的資料
 function get_json_input()
 {
     $data = file_get_contents("php://input");
     return json_decode($data, true);
 }
 
-
-//回復JOSN訊息
-//state: 狀態(成功或失敗) message: 訊息內容 data: 回傳資料(可有可無)
 function respond($state, $message, $data = null)
 {
     echo json_encode(["state" => $state, "message" => $message, "data" => $data]);
 }
 
-//取得所有資料
-//input: none
-// {"state" : true, "message" : "取得所有位置資料成功"}
-// {"state" : false, "message" : "取得所有位置資料失敗"}
 function get_alldata()
 {
     $conn = create_connection();
@@ -49,7 +40,6 @@ function get_alldata()
         }
         respond(true, "取得所有位置成功", $mydata);
     } else {
-        //查無資料
         respond(false, "查無資料");
     }
     $stmt->close();
@@ -60,7 +50,6 @@ function get_citydata()
 {
     $conn = create_connection();
 
-    // 使用 DISTINCT 過濾重複的 cityName
     $stmt = $conn->prepare("SELECT cityName FROM station_position GROUP BY cityName ORDER BY MIN(id) ASC");
     $stmt->execute();
     $result = $stmt->get_result();
@@ -68,11 +57,10 @@ function get_citydata()
     if ($result->num_rows > 0) {
         $mydata = array();
         while ($row = $result->fetch_assoc()) {
-            $mydata[] = $row['cityName'];  // 只取出 cityName
+            $mydata[] = $row['cityName'];
         }
         respond(true, "取得所有不重複的城市名稱成功", $mydata);
     } else {
-        // 查無資料
         respond(false, "查無資料");
     }
     $stmt->close();
@@ -95,18 +83,8 @@ function count_station()
     $conn->close();
 }
 
-
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $action = $_GET['action'] ?? '';
-    switch ($action) {
-        
-
-        default:
-            respond(false, "無效的操作");;
-    }
-} else if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    $action = $_GET['action'] ?? '';
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+     $action = $_GET['action'] ?? '';
     switch ($action) {
         case 'getalldata':
             get_alldata();
@@ -120,7 +98,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         default:
             respond(false, "無效的操作");
     }
-} else if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+}  else if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
     $action = $_GET['action'] ?? '';
     switch ($action) {
         case 'delete':
