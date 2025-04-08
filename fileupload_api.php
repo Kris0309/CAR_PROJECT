d379a2aa585f531df83e878772e05589ef7844db
Kris0309 <cx90309@gmail.com>
2025-04-08 08:44:25 +0800

    style :排版美化

--- fileupload_api.php
+++ fileupload_api.php
@@ -1,22 +1,18 @@
 <?php
 
-$servername = "192.168.2.3";
-$username = "stone_test";
-$password = "Pks2pZD9LQCYLpCI75k5fw";
-$dbname = "stone_test";
+$servername = "";
+$username = "";
+$password = "";
+$dbname = "";
 
-//建立連線
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-//確認連線
+
 if (!$conn) {
     die("連線錯誤" . mysqli_connect_error());
 }
 
-
-
 if (isset($_FILES['file']['name']) && $_FILES['file']['name'] != "" && isset($_POST["id"]) && $_POST["id"] != "") {
     if ($_FILES['file']['type'] == 'image/jpeg' || $_FILES['file']['type'] == 'image/png') {
-        //檔案(圖片)上傳至伺服器(後端主機)
         $filename = date("YmdHis") . "_" . $_FILES['file']['name'];
         $id = $_POST["id"];
 
@@ -34,11 +30,9 @@ if (isset($_FILES['file']['name']) && $_FILES['file']['name'] != "" && isset($_P
             $datainfo["error"] = $_FILES['file']['error'];
             echo json_encode($datainfo);
 
-            // 使用預處理語句防止 SQL 注入
             $stmt = $conn->prepare("UPDATE car SET Photo = ? WHERE id = ?");
             $stmt->bind_param("si", $filename, $id);
 
-            // 執行更新
             $stmt->execute();
             $stmt->close();
             $conn->close();
